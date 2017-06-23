c3632208ea1049bffbf806cd1a34a7a201996c82
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-06-23 09:04:44 -0700

    Add .gitignore

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,57 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
 	public Text countText;
+	public Text timeRemainingDisplayText;
 	public Text winText;
+//	public GameObject roundEndDisplay;
+
+	private DataController dataController;
+	private RoundData currentRoundData;
+
+	private bool isRoundActive;
+	private float timeRemaining;
+
 
 	private Rigidbody rb;
 	private int count;
 
 	void Start ()
 	{
+		dataController = FindObjectOfType<DataController> ();
+		currentRoundData = dataController.GetCurrentRoundData ();
+		timeRemaining = currentRoundData.timeLimitInSeconds;
+		UpdateTimeRemainingDisplay();
+
 		rb = GetComponent<Rigidbody> ();	
 		count = 0;
 		SetCountText ();
 		winText.text = "";
+		isRoundActive = true;
+
+	}
+
+	void Update () 
+	{
+		if (isRoundActive) 
+		{
+			timeRemaining -= Time.deltaTime;
+			UpdateTimeRemainingDisplay();
+
+			if (timeRemaining <= 0f)
+			{
+				EndRound();
+			}
+
+		}
 	}
 		
 	void FixedUpdate ()
 	{
-//		float moveHorizontal = Input.GetAxis ("Horizontal");
-//		float moveVertical = Input.GetAxis ("Vertical");
-
-//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		Vector3 movement = new Vector3 (Input.acceleration.x, 0.0f, Input.acceleration.y);
 		rb.AddForce (movement * speed);
@@ -39,12 +67,30 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+	public void EndRound()
+	{
+		isRoundActive = false;
+	}
+
+	private void UpdateTimeRemainingDisplay()
+	{
+		timeRemainingDisplayText.text = "Time: " + Mathf.Round (timeRemaining).ToString ();
+	}
+
 	void SetCountText ()
 	{
 		countText.text = "Count: " + count.ToString ();
 		if (count >= 12)
 		{
 			winText.text = "You win!";
+			ReturnToMenu ();
 		}
 	}
+
+
+
+	void ReturnToMenu ()
+	{
+		SceneManager.LoadScene ("MenuScreen");
+	}		
 }
